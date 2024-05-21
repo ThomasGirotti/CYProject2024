d9d8d318cae2f5c0cfdcc57afc3682cbfa41fdbe
7-eElement <164475725+7-eElement@users.noreply.github.com>
2024-05-21 11:44:11 +0200

    Update PointView.java

--- PointView.java
+++ PointView.java
@@ -1,57 +1,70 @@
-package ui;
-
-import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.input.MouseEvent;
+import javafx.event.*;
+import javafx.scene.control.Label;
 
 public class PointView {
-    private int orgSceneX, orgSceneY;
-    private int orgTranslateX, orgTranslateY;
+	double orgSceneX, orgSceneY;
+    double orgTranslateX, orgTranslateY;
     private String name;
     private Circle circle;
-    private Label label;
     private Point point;
-
-    public PointView(int x, int y, String name) {
+    private Label label;
+    public PointView(double x, double y, String name) {
         this.name = name;
-        this.point = new Point(x, y, name);
-        this.circle = new Circle(x, y, 5d); // Crée un cercle avec le rayon de 5 pixels
+
+    	this.circle = new Circle(x, y, 5d); // Crée un cercle avec le rayon de 10 pixels
         this.circle.setFill(Color.RED); // Définit la couleur de remplissage à rouge
+        this.label = new Label(name);
+        this.label.setLayoutX(circle.getCenterX() + circle.getTranslateX() + circle.getRadius() + 5);
+        this.label.setLayoutY(circle.getCenterY() + circle.getTranslateY() - 10);
+        this.point = new Point(x, y, name);
+    }
+    
+    public Label getLabel(){
+        return this.label;
+    }
+
+    public void setLabel(Label label) {
+        this.label = label;
     }
 
     public Circle getCircle() {
-        return this.circle;
+    	return this.circle;
     }
-
+    
     public String getName() {
-        return this.name;
+    	return this.name;
     }
 
-    public Point getPoint() {
-        return this.point;
+    public Point getPoint(){
+        return point;
     }
-
+    
     public void setX(double x) {
         circle.setCenterX(x);
+        point.setX(x);
     }
 
     public void setY(double y) {
         circle.setCenterY(y);
+        point.setY(y);
     }
 
-    public void setOrgSceneX(int orgSceneX) {
+    public void setOrgSceneX(double orgSceneX) {
         this.orgSceneX = orgSceneX;
     }
 
-    public void setOrgSceneY(int orgSceneY) {
+    public void setOrgSceneY(double orgSceneY) {
         this.orgSceneY = orgSceneY;
     }
 
-    public void setOrgTranslateX(int orgTranslateX) {
+    public void setOrgTranslateX(double orgTranslateX) {
         this.orgTranslateX = orgTranslateX;
     }
 
-    public void setOrgTranslateY(int orgTranslateY) {
+    public void setOrgTranslateY(double orgTranslateY) {
         this.orgTranslateY = orgTranslateY;
     }
 
@@ -62,25 +75,13 @@ public class PointView {
     public double getOrgTranslateY() {
         return orgTranslateY;
     }
-
+    
     public double getOrgSceneX() {
         return orgSceneX;
     }
-
+    
     public double getOrgSceneY() {
         return orgSceneY;
     }
 
-    public Label getLabel() {
-        return label;
-    }
-
-    public void setLabel(Label label) {
-        this.label = label;
-    }
-
-    public void updatePointCoordinates() {
-        this.point.setX((int) (circle.getCenterX() + circle.getTranslateX()));
-        this.point.setY((int) (circle.getCenterY() + circle.getTranslateY()));
-    }
 }
